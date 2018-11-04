b2c8faf86a199106e0e27b5578fb22a734639a22
unknown <clayton.weller@gmail.com>
2018-11-04 15:37:24 -0700

    Even more integration of front and back end

--- communication/router.js
+++ communication/router.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const { SENDGRID_KEY } = require("../config");
 const sgMail = require("@sendgrid/mail");
 
-console.log(SENDGRID_KEY);
-
 const { jwtStrategy } = require("../auth");
+const { SENDGRID_KEY } = require("../config");
+const db = require("../db");
+const { findUserById } = require("../users/models");
+
+console.log(SENDGRID_KEY);
 
 const jwtAuth = passport.authenticate("jwt", { session: false });
 
@@ -18,21 +20,24 @@ router.use(jsonParser);
 sgMail.setApiKey(SENDGRID_KEY);
 
 router.post("/", jwtAuth, (req, res) => {
-  console.log(req.body);
-  const msg = {
-    to: req.body.toEmail,
+  let msg = {
     from: req.user.email,
     subject: req.body.subject,
     text: `${
-      req.body.text
+      req.body.contact
     }. --- This Message was sent through Work With Artists (WWA). If you would like to opt out of receiving emails from WWA please visit this link: LINK`,
     html: `<h1>Work With Artists</h1>
-      <div>${req.body.text}</div> 
+      <div>${req.body.contact}</div>
       <div> --- This Message was sent through Work With Artists (WWA). If you would like to opt out of receiving emails from WWA please visit this link: LINK</div>`
   };
-  sgMail
-    .send(msg)
-    .then(_res => res.status(201).json(_res))
+
+  db.query(findUserById(req.body.artistId))
+    .then(dbres => (msg.to = dbres.rows[0].email))
+    .then(() => {
+      console.log(msg);
+      return sgMail.send(msg);
+    })
+    .then(_res => res.status(201).json({ message: "Sent" }))
     .catch(err => {
       console.log(err);
       res.status(500).json({ message: "Something went wrong on the server" });
