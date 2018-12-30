c5751be6167f2cb50f5c665634b910b734245ea6
unknown <clayton.weller@gmail.com>
2018-12-30 16:34:12 -0700

    Fixed the image upload fetch bug

--- users/images.js
+++ users/images.js
@@ -3,31 +3,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cloudinary = require("cloudinary");
 const formData = require("express-form-data");
-
-// const passport = require("passport");
+const passport = require("passport");
 const {
   CLOUDINARY_NAME,
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET
 } = require("../config");
 
-// const {
-//   findUserById,
-//   findUnfinishedUserById,
-//   findUserByEmail,
-//   findUsersByDiscipline,
-//   findAllUsers,
-//   createUser,
-//   hashPassword,
-//   fixSingleQuotesForSQL
-// } = require("./models");
-// const db = require("../db");
-// const { jwtStrategy } = require("../auth");
+const db = require("../db");
+const { jwtStrategy } = require("../auth");
 
-// const jwtAuth = passport.authenticate("jwt", { session: false });
+const jwtAuth = passport.authenticate("jwt", { session: false });
 const router = express.Router();
 
-// passport.use(jwtStrategy);
+passport.use(jwtStrategy);
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(formData.parse());
@@ -38,14 +27,21 @@ cloudinary.config({
   api_secret: CLOUDINARY_API_SECRET
 });
 
-router.post("/", (req, res) => {
-  let imageFile = req.body;
-  console.log(req.files);
-  console.log(imageFile);
-
-  res.status(200).json(req.body);
-
-  // cloudinary.uploader.upload()
+router.post("/", jwtAuth, (req, res) => {
+  let userId = req.user.user_id;
+  cloudinary.uploader
+    .upload(req.files.imageFile.path)
+    .then(res => {
+      return db.query(
+        `
+          UPDATE users
+          SET img_url = '${res.url}'
+          WHERE user_id = '${userId}'
+        `
+      );
+    })
+    .then(() => res.status(200))
+    .catch(err => console.log(err));
 });
 
 module.exports = { router };
